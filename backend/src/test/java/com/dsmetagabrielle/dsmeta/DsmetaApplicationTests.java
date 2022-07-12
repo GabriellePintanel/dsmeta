ae9eed5bc1d5d97d71b0fe823b576b63a0b88a00
Gabrielle Pintanel <gabrielle.fpintanel@gmail.com>
2022-07-12 02:33:52 -0300

    Project created

---
+++ backend/src/test/java/com/dsmetagabrielle/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsmetagabrielle.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
