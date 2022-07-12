45752a0a52542caea2dcfbbbd065159e67e296af
Gabrielle Pintanel <107973290+GabriellePintanel@users.noreply.github.com>
2022-07-12 03:10:31 -0300

    Main1 (#3)
    * Project clean
    
    * First component
    
    * Other Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
- main
+main
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 import NotificationButton from "./components/NotificationButton"
+main
 
 function App() {
   return (
     <>
-      <h1>Olá mundo!</h1>
-      <NotificationButton />
+main
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
 main
   )
